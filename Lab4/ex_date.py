ea6776a8d42b1b97951f74096d9246a194cdc47a
Azat <allnforbs@gmail.com>
2024-02-25 20:44:33 +0600

    all tasks

--- Lab4/ex_date.py
+++ Lab4/ex_date.py
@@ -1,19 +1,25 @@
-from datetime import datetime , timedelta 
-#task1
+import json
 
-x = datetime.now() - timedelta(days=5)
-y = x.strftime("%A")
-print(x,y)
+with open("sample_data.json", "r") as my_file:
+    json_string = my_file.read()
+
+data = json.loads(json_string)
+
+interfaces = data.get('imdata', [])
 
 
-#task2
-x = datetime.now() + timedelta(days=1)
-x2 = datetime.now() - timedelta(days=1)
-x3 = datetime.now()
-y = x.strftime("%A")
-y2 = x2.strftime("%A")
-y3 = x3.strftime("%A")
-print(x,y)
-print(x2,y2)
-print(x3,y3)
+print("Interface Status")
+print("=" * 80)
+print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
+print("-" * 50, "-" * 20, "-" * 8, "-" * 6)
 
+for interface in interfaces[1:]:
+    l1_phys_if = interface.get('l1PhysIf', {})
+    attributes = l1_phys_if.get('attributes', {})
+    
+    dn = attributes.get('dn', '')
+    description = attributes.get('descr', '')
+    speed = attributes.get('speed', 'inherit')
+    mtu = attributes.get('mtu', '')
+    
+    print("{:<50} {:<20} {:<8} {:<6}".format(dn, description, speed, mtu))
